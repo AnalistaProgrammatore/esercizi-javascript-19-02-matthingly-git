d912d54bf8e7a6d56949789d0b6b9c9e99267212
matthingly-git <matthew.cesar96@gmail.com>
2020-02-23 21:57:08 +0100

    Primo commit

--- range.js
+++ range.js
@@ -1,3 +1,15 @@
 /**
 * Scrivere qui il codice dell'esercizio 1 del capitolo 4 di eloquentJS
 **/
+
+const x = function range(start, end) {
+  let arrayRange = [];
+  while (start <= end) {
+    arrayRange.push(start);
+    start++;
+  }
+  return arrayRange;
+}
+
+let y = x(1, 27);
+console.log(y);
