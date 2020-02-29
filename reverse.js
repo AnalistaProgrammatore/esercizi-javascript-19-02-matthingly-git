ae9db6113060525d32d5c4cab5299dee7dbd0f4e
matthingly-git <matthew.cesar96@gmail.com>
2020-02-29 18:51:59 +0100

    Manca dichiarativa

--- reverse.js
+++ reverse.js
@@ -2,3 +2,19 @@
 * Scrivere qui il codice dell'esercizio 2 del capitolo 4 di eloquentJS
 * Vi ricordo che le funzioni DEVONO ESSERE PURE e implementate una in modo IMPERATIVO e una in modo DICHIARATIVO
 **/
+
+
+//IMPERATIVO
+function reverseArray (array) {
+  let arrayCopied = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    arrayCopied.push(array[i])
+  }
+  return arrayCopied;
+}
+
+console.log(reverseArray([1, 2, 3, 4, 5]));
+
+
+
+//DICHIARATIVO
